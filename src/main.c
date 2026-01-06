1581f67719e974344d9c95427533fd6abe77b23d
Joe Gray <joegray122@gmail.com>
2026-01-06 16:52:17 +0000

    feat: test database headers

--- src/main.c
+++ src/main.c
@@ -8,8 +8,78 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n - create a new database file\n");
+	printf("\t -f (required) - path to database file\n");
+	return;
 }
 
 int main(int argc, char *argv[]) { 
-	
+	char *filepath = NULL;
+	char *portarg = NULL;
+	unsigned short port = 0;
+	bool newfile = false;
+	bool list = false;
+	int c;
+
+	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
+
+	while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
+		switch (c) {
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case 'p':
+				portarg = optarg;
+				break;
+			case 'l':
+				list = true;
+				break;
+			case '?':
+				printf("Unknown option -%c\n", c);
+				break;
+			default:
+				return -1;
+
+		}
+	}
+
+	if (filepath == NULL) {
+		printf("Filepath is a required argument\n");
+		print_usage(argv);
+
+		return 0;
+	}
+
+	if (newfile) {
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to create database file\n");
+			return -1;
+		}
+
+		if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to create database header\n");
+			return -1;
+		}
+	} else {
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to open database file\n");
+			return -1;
+		}
+
+		if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to validate database header\n");
+			return -1;
+		}
+	}
+
+	output_file(dbfd, dbhdr);
+
+	return 0;
 }
