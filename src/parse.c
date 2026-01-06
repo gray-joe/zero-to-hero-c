9f8a23a07004e04915e657ef2d497f68e782b8cb
Joe Gray <joegray122@gmail.com>
2026-01-06 20:54:59 +0000

    fix: filesize header

--- src/parse.c
+++ src/parse.c
@@ -10,18 +10,6 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
-
-}
-
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
-	return 0;
-}
-
-int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
-	return 0;
-}
-
 int output_file(int fd, struct dbheader_t *dbhdr) {
 	if (fd < 0) {
 		printf("Recieved a bad FD from user\n");
@@ -31,24 +19,25 @@ int output_file(int fd, struct dbheader_t *dbhdr) {
 	dbhdr->version = ntohs(dbhdr->version);
 	dbhdr->magic = ntohl(dbhdr->magic);
 	dbhdr->count = ntohs(dbhdr->count);
-	dbhdr->filesize = ntohl(dbhdr->filesize);
+	dbhdr->filesize = ntohl(sizeof(struct dbheader_t));
 
 	lseek(fd, 0, SEEK_SET);
 
 	write(fd, dbhdr, sizeof(struct dbheader_t));
-	return 0;
+	return STATUS_SUCCESS;
 
 }	
 
 int validate_db_header(int fd, struct dbheader_t **headerOut) {
 	if (fd < 0) {
-		printf("Recieved a bad FD from user\n");
+		printf("Received a bad FD from user\n");
 		return STATUS_ERROR;
 	}
 
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-	if (header == -1) {
+	if (header == NULL) {
 		printf("Malloc failded to create db header\n");
+		return STATUS_ERROR;
 	}
 
 	if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t)) {
@@ -83,14 +72,14 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 	}
 
 	*headerOut = header;
-
-	return 0;
+	return STATUS_SUCCESS;
 }
 
 int create_db_header(int fd, struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-	if (header == -1) {
+	if (header == NULL) {
 		printf("Malloc failded to create db header\n");
+		return STATUS_ERROR;
 	}
 
 	header->version = 0x1;
@@ -101,7 +90,5 @@ int create_db_header(int fd, struct dbheader_t **headerOut) {
 	*headerOut = header;
 
 	return STATUS_SUCCESS;
-	
 }
-
 
